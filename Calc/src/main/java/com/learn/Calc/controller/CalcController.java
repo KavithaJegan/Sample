8666fce69e81385700b716d69185d19f4c307659
kavitha <kavithajeganathan.tech@gmail.com>
2025-02-10 13:22:31 -0600

    Initial Commit

---
+++ Calc/src/main/java/com/learn/Calc/controller/CalcController.java
@@ -0,0 +1,26 @@
+package com.learn.Calc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.apache.catalina.util.*;
+
+@RestController
+public class CalcController {
+
+	
+	
+	@GetMapping("/add")
+	public int add(@RequestParam int a, @RequestParam int b) {
+		return a + b;
+		
+	}
+	
+	@GetMapping("/sub/{a}/{b}")
+	public int sub(@PathVariable int a, @PathVariable int b) {
+		return a - b;
+	}
+	
+//	StringUtils.
+}
