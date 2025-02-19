4fb399acc06ca7d8f35a60c5a830f11166acd2e2
kavitha <kavithajeganathan.tech@gmail.com>
2025-02-19 16:33:07 -0600

    Try commit

--- Calc/src/main/java/com/learn/Calc/controller/CalcController.java
+++ Calc/src/main/java/com/learn/Calc/controller/CalcController.java
@@ -1,4 +1,4 @@
-package com.learn.Calc.controller;
+package src.main.java.com.learn.Calc.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +21,7 @@ public class CalcController {
 	
 	@GetMapping("/sub/{a}/{b}")
 	public int sub(@PathVariable int a, @PathVariable int b) {
+		System.out.println("This is a mandatory line");
 		return a - b;
 	}
 	
